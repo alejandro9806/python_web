0003a456b348033304aa8dd94e5d410d756a3651
alejandro9806 <Jbuitragom04@gmail.com>
2025-02-05 11:29:49 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def servicios():
 # Configuración de Flask-Mail con variables de entorno
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
-app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+app.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'False') == 'False'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_DEFAULT_SENDER')
